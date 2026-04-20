b990212ad184c7969c84dd355da2191494e0b8ec
Seth Pyle <seth@sethpyle.com>
2026-04-20 18:49:38 -0500

    default log level

--- hiraeth_runtime/src/main.rs
+++ hiraeth_runtime/src/main.rs
@@ -55,8 +55,9 @@ fn aws_endpoint_url(config: &hiraeth_core::Config) -> String {
 }
 
 fn init_tracing() {
-    let env_filter = EnvFilter::try_from_default_env()
-        .unwrap_or_else(|_| EnvFilter::new("hiraeth_runtime=info,hiraeth_web=info"));
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| {
+        EnvFilter::new("hiraeth_runtime=info,hiraeth_web=info,hiraeth_iam=info")
+    });
 
     fmt().with_env_filter(env_filter).init();
 }
